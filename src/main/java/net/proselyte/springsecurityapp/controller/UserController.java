ad255db2e6959e766ace0efa9a039979e191e18e
miromax21 <miromax21@gmail.com>
2018-03-30 13:36:29 +0400

    *fix auth

--- src/main/java/net/proselyte/springsecurityapp/controller/UserController.java
+++ src/main/java/net/proselyte/springsecurityapp/controller/UserController.java
@@ -1,18 +1,20 @@
 package net.proselyte.springsecurityapp.controller;
 
+import net.proselyte.springsecurityapp.model.Article;
 import net.proselyte.springsecurityapp.model.User;
 import net.proselyte.springsecurityapp.service.interfaces.SecurityService;
 import net.proselyte.springsecurityapp.service.interfaces.UserService;
 import net.proselyte.springsecurityapp.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class UserController {
 
     @Autowired
@@ -46,17 +48,22 @@ public class UserController {
         return "redirect:/welcome";
     }
 
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String login(Model model, String error, String logout) {
-        if (error != null) {
-            model.addAttribute("error", "Username or password is incorrect.");
-        }
 
-        if (logout != null) {
-            model.addAttribute("message", "Logged out successfully.");
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ResponseEntity<User> createArticle(@RequestBody User user) {
+        if(user == null){
+            return  new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-
-        return "login";
+//        if (error != null) {
+//            model.addAttribute("error", "Username or password is incorrect.");
+//        }
+//
+//        if (logout != null) {
+//            model.addAttribute("message", "Logged out successfully.");
+//        }
+       // User user1
+        return  new ResponseEntity<User>(user, HttpStatus.OK);
+      //  return "login";
     }
 
     @RequestMapping(value = {"/", "/welcome"}, method = RequestMethod.GET)
